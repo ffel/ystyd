c00107c9f6fab16bd58c30a9861d7b912051db89
Mastermindzh <rick@mi-soft.nl>
2016-02-22 16:52:30 +0100

    insertSiteNav refactoring

--- ystyd.go
+++ ystyd.go
@@ -27,51 +27,45 @@ func main() {
 func insertSiteNav(dir, cfile string) {
 	fmt.Printf("post process %q with config file %q\n", dir, cfile)
 
-	// read config
-	yamlbytes, err := ioutil.ReadFile(cfile)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// create Site
-	site := postprocess.NewSite()
-	err = site.Read(string(yamlbytes))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// read all html files in dir and add the menu
-	files, err := ioutil.ReadDir(dir)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	readConfig(cfile)
+	createSite()
+	addPages(dir)
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) == ".html" {
 			// read html file with {{.Nav}}
 			templateBytes, err := ioutil.ReadFile(dir + "/" + f.Name())
-
-			if err != nil {
-				log.Fatal(err)
-			}
+			checkForErrors(err)
 
 			// insert Nav menu
 			html, err := site.PostProcess(f.Name(), string(templateBytes))
-
-			if err != nil {
-				log.Fatal(err)
-			}
+			checkForErrors(err)
 
 			// write the html file
 			err = ioutil.WriteFile(dir+"/"+f.Name(), []byte(html), 0644)
-
-			if err != nil {
-				log.Fatal(err)
-			}
+			checkForErrors(err)
 		}
 	}
 
+}
+
+func readConfig(cfile string) {
+	yamlbytes, err := ioutil.ReadFile(cfile)
+	checkForErrors(err)
+}
+func createSite() {
+	site := postprocess.NewSite()
+	err = site.Read(string(yamlbytes))
+	checkForErrors(err)
+}
+
+// read all html files in dir and add the menu
+func addPages(dir) {
+	files, err := ioutil.ReadDir(dir)
+	checkForErrors(err)
+}
+func checkForErrors(err) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
